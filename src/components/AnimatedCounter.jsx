380ab8d4458e43c5fe7ff1525ffd258dda513b8c
salman09-prog <salmanyusufzai48@gmail.com>
2025-10-11 18:05:57 +0530

    Updated real stats

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -4,7 +4,7 @@ import CountUp from "react-countup";
 const AnimatedCounter = () => {
   return (
     <div id="counter" className="padding-x-lg xl:mt-0 mt-32">
-      <div className="mx-auto grid-4-cols">
+      <div className="mx-auto grid-2-cols">
         {counterItems.map((item) => (
           <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
             <div
